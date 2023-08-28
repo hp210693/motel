c9670df22c31b04c3b637f0292b2adfa78f10d14
hungphan <hungvanphan93@gmail.com>
2023-08-28 20:16:28 +0700

    test

--- motel-backend/main.go
+++ motel-backend/main.go
@@ -24,18 +24,19 @@ SOFTWARE.
 package main
 
 import (
-	"motel-backend/config"
-	delivery "motel-backend/delivery"
-	service "motel-backend/service"
-
-	infrast "motel-backend/infrast/postgress"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 
-	// Connect To Database
+	/* // Connect To Database
 	config.DatabaseInit()
 	gorm := config.DB()
 
@@ -64,5 +65,5 @@ func main() {
 	billService := service.NewBillService(billInfrast)
 	delivery.NewBillDelivery(echoContext, billService)
 
-	echoContext.Logger.Fatal(echoContext.Start(":8080"))
+	echoContext.Logger.Fatal(echoContext.Start(":8080")) */
 }
